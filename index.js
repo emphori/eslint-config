2030a8751fa5c4f3f5e4f1e73feba0b678d9e59f
Iain Reid <iainreid382@gmail.com>
2018-04-23 16:05:40 +0000

    Add initial rules

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 'use strict';
 
 module.exports = {
+  rules: {
+    'valid-jsdoc': ['error', {
+      prefer: {
+        returns: 'return'
+      }
+    }],
+    'block-scoped-var': 'error',
+    'class-methods-use-this': 'error',
+    'no-empty-function': 'error',
+    'no-eval': 'error',
+    'no-new-func': 'error'
+    'no-new-wrappers': 'error',
+    'no-param-reassign': ['error', {
+      props: true
+    }],
+    'no-return-await': 'error',
+    'require-await': 'error',
+    'yoda': ['error', 'never', { exceptRange: true }],
+    'strict': ['error', 'global']
+  },
   extends: 'eslint:recommended'
 };
 
