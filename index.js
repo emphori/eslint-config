04b8e2b339988b24932e8c02692000e92280d396
Iain Reid <iainreid382@gmail.com>
2019-09-04 10:37:08 +0000

    Add Promise recognition

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 module.exports = {
   env: {
     node: true,
+    es6: true,
   },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 9,
   },
   rules: {
     'block-scoped-var': 'error',
